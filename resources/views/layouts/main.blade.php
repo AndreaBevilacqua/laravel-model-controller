7afffcfc0399e63168ae98fc37838f09851f21ac
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-11 16:37:39 +0100

    added home and route name

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Controller | @yield('title')</title>
+
+    {{-- Vite --}}
+    @vite('resources/js/app.js')
+
+    {{-- Cdn --}}
+    @yield('cdns')
+</head>
+<body>
+    {{-- Header --}}
+    
+    {{-- Main --}}  
+    <main class="container">
+        @yield('core-content')
+    </main>
+
+    {{-- Footer --}}
+
+    {{-- scripts --}}
+    @yield('scripts')
+    
+</body>
+</htmle

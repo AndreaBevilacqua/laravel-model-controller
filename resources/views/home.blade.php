3fa440b1b8d428d975f7094c8f59aae58b9cef14
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-11 17:07:46 +0100

    fixed foelse

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,20 @@
 <section id="movies">
     <div class="row">
         @forelse($movies as $movie)
-        <div class="col-2">
-            <div class="card">
+        <div class="col-6">
+            <div class="card mb-3">
                 <div class="card-body">
                   <h5 class="card-title">{{$movie->title}}</h5>
                   <h6 class="card-subtitle mb-2 text-body-secondary">{{$movie->nationality}}</h6>
-                  <p class="card-text">{{$movie->date}} - {{$movie->vote}}</p>
+                  <p class="card-text">Data: {{$movie->date}}  Voto: {{$movie->vote}}</p>
                 </div>
               </div>
         </div>
+        @empty 
+        <div class="col-12">
+            <h2>Non ci sono film</h2>
+        </div>
+        @endforelse
     </div>
 </section>
 
